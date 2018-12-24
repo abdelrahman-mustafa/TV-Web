2b249e5e176b18a953a1dd287d63568cb5ccb826
boda <abdelrahmanmustafa00@gmail.com>
2018-12-24 19:54:37 +0200

    update static folder

--- server.js
+++ server.js
@@ -4,9 +4,8 @@ const path = require('path')
 const express = require('express');
 const config = require('./src/config/config')
 
-server.express.use(express.static(path.resolve(__dirname, 'static')))
+// server.express.use(express.static(path.resolve(__dirname, 'static')))
 
-server.express.use('/', (req, res) => res.sendFile(path.resolve(__dirname, './static/Greek.html')))
 const helmet = require('helmet')
 const compression = require('compression')
 
