f9c7445452c6f784a8ec0b226fe14741c589c34c
boda <abdelrahmanmustafa00@gmail.com>
2019-01-08 00:57:42 +0200

    add front end

--- server.js
+++ server.js
@@ -4,17 +4,22 @@ const path = require('path')
 const express = require('express');
 const config = require('./src/config/config')
 
-// server.express.use(express.static(path.resolve(__dirname, 'static')))
+server.express.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+server.express.get('/', (req, res) => res.redirect('/tv/client'));
+server.express.use(express.static(path.resolve(__dirname, 'static')))
+server.express.use('/tv/client', (req, res) => res.sendFile(path.resolve(__dirname, './static/tv/client/index.html')));
+server.express.use('/tv/admin', (req, res) => res.sendFile(path.resolve(__dirname, './static/tv/admin/index.html')));
 
 const helmet = require('helmet')
 const compression = require('compression')
-
 server.express.use(helmet())
 server.express.use(compression())
-
 server.express.use(config.limiter)
-
-
 server.start(config.options, ({ port }) => {
     console.log(`Server running at http://localhost:${port}`)
-  })
+})
