9d76ae207c6604e4dc4f55128b201bfe6da02419
boda <abdelrahmanmustafa00@gmail.com>
2019-01-18 17:40:18 +0200

    resolvers

--- src/models/day/dayRes.js
+++ src/models/day/dayRes.js
@@ -6,7 +6,22 @@ const day = {
         day: async (_, args, context, info)=>{
             return await context.prisma.query.day({...args},info)
         },
-    }
+    },
+    Mutation: {
+        createDay : async (_,args, context, info)=>{
+            const y = await  context.prisma.mutation.createDay({...args},info)
+
+          return y;
+        },
+        updateDay : async (_,args, context, info)=>{
+            const y = await  context.prisma.mutation.updateDay({...args},info)
+            return y;
+        },
+        deleteDay: async (_,args, context, info)=>{
+            const y = await  context.prisma.mutation.deleteDay({...args},info)
+            return y;
+        }
+    },
 }
 
 module.exports= day;
