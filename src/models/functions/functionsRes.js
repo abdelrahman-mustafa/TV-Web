f96f63446d9837bf46442174f1ed7843ab90bf03
boda <abdelrahmanmustafa00@gmail.com>
2019-01-24 11:33:15 +0200

    update

---
+++ src/models/functions/functionsRes.js
@@ -0,0 +1,30 @@
+const infoData = {
+    Query: {
+        search: async (_, args, context, info)=>{
+            let result = []
+            const articles =  await context.prisma.query.articles({
+                where:{
+                    name_contains: args.keyword,
+                    OR:{
+                        description_contains:args.keyword
+                    }
+                }
+            },info)
+            const sportsArticles =  await context.prisma.query.sportsArticles({
+                where:{
+                    name_contains: args.keyword,
+                    OR:{
+                        description_contains:args.keyword
+                    }
+                }
+            },info)
+            await result.push(articles)
+            await result.push(sportsArticles)
+            return result
+
+        },
+        
+    },
+}
+
+module.exports= infoData;
