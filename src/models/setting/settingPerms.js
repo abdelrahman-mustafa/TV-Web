5aee418d1677b9995727133a22eb1de3e4743498
boda <abdelrahmanmustafa00@gmail.com>
2019-02-05 00:05:27 +0200

    update

--- src/models/setting/settingPerms.js
+++ src/models/setting/settingPerms.js
@@ -3,8 +3,6 @@ const { isAuthenticated } = require('../../middleware/isAuthenticated')
 module.exports = {
     Mutation: {
         updateSetting: isAuthenticated,
-        createSetting: isAuthenticated,
-        deleteSetting: isAuthenticated
     }
 }
 
