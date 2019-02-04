679da09f5dd783004bfe7e6e7facb6dba6bfe0b8
boda <abdelrahmanmustafa00@gmail.com>
2019-02-04 08:30:11 +0200

    update permission for admin

--- src/models/admin/adminPerms.js
+++ src/models/admin/adminPerms.js
@@ -2,6 +2,6 @@ const { isAuthenticated } = require('./../../middleware/isAuthenticated')
 
 module.exports = {
     Query :{admins: isAuthenticated,admin:isAuthenticated},
-    Mutation: {createAdmin: isAuthenticated}
+    Mutation: {updateAdmin: isAuthenticated}
 }
 
