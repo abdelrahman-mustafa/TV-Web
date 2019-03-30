e1c1f927ff9821676688ba2825ce8c5a35965f96
Abdelrahman Saleh <a.saleh@cequens.com>
2019-03-30 20:31:58 +0200

    update server port

--- static/tv/client/main.js
+++ static/tv/client/main.js
@@ -4821,7 +4821,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 var environment = {
     production: false,
-    IP: "http://159.100.130.43:5000",
+    IP: "http://159.100.130.43:5000:5000",
 };
 /*
  * In development mode, to ignore zone related error stack frames such as
