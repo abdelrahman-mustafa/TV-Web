91d6e9044060bd8bec23c1e276bdff02987cf379
Abdelrahman Saleh <a.saleh@cequens.com>
2019-09-19 20:41:37 +0200

    update

--- static/admin/main.js
+++ static/admin/main.js
@@ -9084,7 +9084,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 var environment = {
     production: false,
-    IP: "http://159.100.130.43",
+    IP: "http://localhost",
     firebase: {
         apiKey: "AIzaSyD7AVHMepyh0zHWrsT7dmFLMnlDTE9yeLs",
         authDomain: "wtvproject-f33c2.firebaseapp.com",
