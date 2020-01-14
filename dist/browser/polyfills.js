4c264b07dcefc2dc2b5acc6a11d8c074ff18857e
khalid.naser2323 <khalid.naser2323@gmail.com>
2020-01-14 16:03:42 +0200

    Make links editable, enhance search view

--- dist/browser/polyfills.js
+++ dist/browser/polyfills.js
@@ -5473,7 +5473,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! D:\wtv2\tvclient\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/a/Work/WTV/tvclient/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
